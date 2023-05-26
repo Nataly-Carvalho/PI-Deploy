7505a7904344af39cdc35809b2fece79281d397b
Nataly <natalycarvalho404@gmail.com>
2023-05-26 10:32:32 -0300

    first commit

---
+++ src/main/java/com/OddinaryCosmo/UPV/Repository/UsuarioRepository.java
@@ -0,0 +1,22 @@
+package com.OddinaryCosmo.UPV.Repository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.OddinaryCosmo.UPV.Model.UsuarioModel;
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long>{
+	public Optional<UsuarioModel> findByUsuario(@Param("usuario")String usuario);
+	
+	
+	public List <UsuarioModel> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+}
---
+++ src/main/java/com/OddinaryCosmo/UPV/UpvApplication.java
@@ -0,0 +1,13 @@
+package com.OddinaryCosmo.UPV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpvApplication.class, args);
+	}
+
+}
